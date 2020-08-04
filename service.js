5a3aa4168c82e9aa29c89cb5fa1d8a3fdfcb850e
haishi <haishi@haishi.local>
2020-08-04 11:35:03 +0900

    worker for cache.

--- service.js
+++ service.js
@@ -1,11 +1,31 @@
-self.addEventListener('install', event => {
+var CACHE_NAME = 'quiz-sample-caches';
+var urlsToCache = [
+  '/haishi-at-mediowl.github.io/',
+];
+
+self.addEventListener('install', function(event) {
   console.log('installed.');
+  event.waitUntil(
+    caches
+    .open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetched.');
+  event.respondWith(
+    caches
+    .match(event.request)
+    .then(function(response) {
+      return response ? response : fetch(event.request);
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
   console.log('activated');
 });
 
-self.addEventListener('fetch', event => {
-  console.log('fetched.');
-});
